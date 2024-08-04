2ace84b51f95922df7fe3d1cfc04e9d7243539e2
Nikita88575 <kekchekcebyrek@gmail.com>
2024-08-04 04:08:19 +0300

    Added project bio

--- static/js/main.js
+++ static/js/main.js
@@ -11,3 +11,16 @@ $(document).on("click", ".go-to-about-me", function(about) {
     const cord = $(id).offset().top; 
     $('body, html').animate({scrollTop: cord}, 1500);
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
